ea0330c5dff9ffdb3b5a4760d597ebbcc1c168a9
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-09-07 22:27:59 +0300

    sys/feature: generate Rust source code from C header files

---
+++ couchbase-lite-core-sys/src/c4_header.rs
@@ -0,0 +1,11 @@
+#![allow(
+    unknown_lints,
+    non_upper_case_globals,
+    dead_code,
+    non_camel_case_types,
+    improper_ctypes,
+    non_snake_case,
+    clippy::all
+)]
+
+include!(concat!(env!("OUT_DIR"), "/c4_header.rs"));

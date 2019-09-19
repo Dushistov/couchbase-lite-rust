1d812a89b16a6e31bac2adcbd6dbb13bd0327853
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-09-19 05:43:14 +0300

    little fix: LogNone => Error log level

--- couchbase-lite/src/log_reroute.rs
+++ couchbase-lite/src/log_reroute.rs
@@ -1,10 +1,24 @@
+#![allow(non_upper_case_globals)]
+
 use crate::ffi::{
-    c4log_setRustCallback, kC4DatabaseLog, kC4DefaultLog, kC4LogDebug, kC4LogError, kC4LogInfo,
-    kC4LogNone, kC4LogVerbose, kC4LogWarning, kC4QueryLog, kC4SyncLog, kC4WebSocketLog,
+    c4log_setRustCallback, kC4DatabaseLog, kC4DefaultLog, kC4QueryLog, kC4SyncLog, kC4WebSocketLog,
     C4LogDomain, C4LogLevel,
 };
 use once_cell::sync::Lazy;
 use std::{ffi::CStr, os::raw::c_char};
+
+macro_rules! define_log_level {
+    ($const_name:ident) => {
+        const $const_name: C4LogLevel = crate::ffi::$const_name as C4LogLevel;
+    };
+}
+
+define_log_level!(kC4LogDebug);
+define_log_level!(kC4LogVerbose);
+define_log_level!(kC4LogInfo);
+define_log_level!(kC4LogWarning);
+define_log_level!(kC4LogError);
+define_log_level!(kC4LogNone);
 
 pub(crate) static DB_LOGGER: Lazy<()> = Lazy::new(|| {
     unsafe { c4log_setRustCallback(kC4LogDebug as C4LogLevel, Some(db_logger_callback)) };
@@ -32,21 +46,15 @@ unsafe extern "C" fn db_logger_callback(
 
     use log::Level::*;
 
-    let level = if level == (kC4LogDebug as C4LogLevel) {
-        Trace
-    } else if level == (kC4LogVerbose as C4LogLevel) {
-        Debug
-    } else if level == (kC4LogInfo as C4LogLevel) {
-        Info
-    } else if level == (kC4LogWarning as C4LogLevel) {
-        Warn
-    } else if level == (kC4LogError as C4LogLevel) {
-        Error
-    } else if level == (kC4LogNone as C4LogLevel) {
-        Info
-    } else {
-        Info
+    let level = match level {
+        kC4LogDebug => Trace,
+        kC4LogVerbose => Debug,
+        kC4LogInfo => Info,
+        kC4LogWarning => Warn,
+        kC4LogError | kC4LogNone => Error,
+        _ => Info,
     };
+
     if !msg.is_null() {
         fn lifetime_marker<'a>(ptr_ref: &'a *const c_char) -> &'a CStr {
             unsafe { CStr::from_ptr(*ptr_ref) }

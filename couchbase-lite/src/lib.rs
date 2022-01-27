a2467f1409ae8e4a02053349411fa20757761757
Evgeniy A. Dushistov <dushistov@mail.ru>
2022-01-28 02:00:54 +0300

    restore back open database functionality

--- couchbase-lite/src/lib.rs
+++ couchbase-lite/src/lib.rs
@@ -1,1 +1,6 @@
+mod database;
+mod error;
 
+pub use crate::database::{Database, DatabaseConfig};
+pub use couchbase_lite_core_sys as ffi;
+pub use ffi::{kC4DB_Create, kC4DB_NoUpgrade, kC4DB_NonObservable, kC4DB_ReadOnly};
--- couchbase-lite/tests/functional_tests.rs
+++ couchbase-lite/tests/functional_tests.rs
@@ -1,1 +1,15 @@
+use couchbase_lite::{kC4DB_Create, Database};
+use tempfile::tempdir;
 
+#[test]
+fn test_write_read() {
+    let _ = env_logger::try_init();
+    let tmp_dir = tempdir().expect("Can not create tmp directory");
+    println!("we create tempdir at {}", tmp_dir.path().display());
+    let db_path = tmp_dir.path().join("a.cblite2");
+    {
+        let mut db = Database::open_with_flags(&db_path, kC4DB_Create).unwrap();
+    }
+
+    tmp_dir.close().expect("Can not close tmp_dir");
+}

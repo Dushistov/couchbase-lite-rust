0baaa17c7cc44330cbfa5a1df26714f67f96994d
Adrien Jeser <adrien.jeser@doctolib.com>
2022-02-02 22:53:21 +0100

    wip

--- couchbase-lite-core-sys/build.rs
+++ couchbase-lite-core-sys/build.rs
@@ -6,7 +6,157 @@ use std::{
     process::Stdio,
 };
 
-fn main() {}
+fn main() {
+    env_logger::init();
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let cross_to_windows = false;
+    let cross_to_macos = false;
+    let cross_to_android = false;
+
+    if cross_to_windows || cross_to_macos {
+        println!("cargo:rustc-link-lib=dylib=LiteCore");
+    } else {
+        if cross_to_android {
+            let dst = cmake::Config::new(Path::new("couchbase-lite-core"))
+                .define("CMAKE_CXX_FLAGS", env::var("CMAKE_CXX_FLAGS").unwrap())
+                .define("CMAKE_C_FLAGS", env::var("CMAKE_C_FLAGS").unwrap())
+                .define("ANDROID_ABI", env::var("ANDROID_ABI").unwrap())
+                .define("ANDROID_NDK", env::var("NDK_HOME").unwrap())
+                .define("CMAKE_BUILD_TYPE", "MinSizeRel")
+                .define(
+                    "ANDROID_NATIVE_API_LEVEL",
+                    env::var("ANDROID_NATIVE_API_LEVEL").unwrap(),
+                )
+                .define("ANDROID_TOOLCHAIN", "clang")
+                .define(
+                    "CMAKE_TOOLCHAIN_FILE",
+                    format!(
+                        "{}/build/cmake/android.toolchain.cmake",
+                        env::var("NDK_HOME").unwrap()
+                    ),
+                )
+                .define("DISABLE_LTO_BUILD", "True")
+                .build_target("LiteCore")
+                .build()
+                .join("build");
+
+            //FIXME When use static also there is a pb of undefined symbol _ZTVN10__cxxabiv120__si_class_type_infoE
+            println!("cargo:rustc-link-search=native={}", dst.display());
+            println!("cargo:rustc-link-lib=dylib=LiteCore");
+        } else {
+            let dst = cmake::Config::new(Path::new("couchbase-lite-core"))
+                .define("DISABLE_LTO_BUILD", "True")
+                .define("MAINTAINER_MODE", "False")
+                .define("ENABLE_TESTING", "False")
+                .define("LITECORE_BUILD_TESTS", "False")
+                .define("SQLITE_ENABLE_RTREE", "True")
+                .build_target("all")
+                .build()
+                .join("build");
+
+            println!("cargo:rustc-link-search=native={}", dst.display());
+            println!(
+                "cargo:rustc-link-search=native={}",
+                dst.join("vendor").join("fleece").display()
+            );
+            println!(
+                "cargo:rustc-link-search=native={}",
+                dst.join("Networking").join("BLIP").display()
+            );
+            println!(
+                "cargo:rustc-link-search=native={}",
+                dst.join("vendor").join("sqlite3-unicodesn").display()
+            );
+            println!(
+                "cargo:rustc-link-search=native={}",
+                dst.join("vendor")
+                    .join("mbedtls")
+                    .join("crypto")
+                    .join("library")
+                    .display()
+            );
+            println!(
+                "cargo:rustc-link-search=native={}",
+                dst.join("vendor").join("mbedtls").join("library").display()
+            );
+            if cfg!(feature = "couchbase-sqlite") {
+                println!("cargo:rustc-link-lib=static=CouchbaseSqlite3");
+            }
+
+            println!("cargo:rustc-link-lib=static=LiteCoreStatic");
+            println!("cargo:rustc-link-lib=static=FleeceStatic");
+            println!("cargo:rustc-link-lib=static=SQLite3_UnicodeSN");
+            println!("cargo:rustc-link-lib=static=BLIPStatic");
+            println!("cargo:rustc-link-lib=static=mbedcrypto");
+            println!("cargo:rustc-link-lib=static=mbedtls");
+            println!("cargo:rustc-link-lib=static=mbedx509");
+        };
+
+        if cfg!(target_os = "linux") {
+            if !cross_to_android {
+                println!("cargo:rustc-link-lib=icuuc");
+                println!("cargo:rustc-link-lib=icui18n");
+                println!("cargo:rustc-link-lib=icudata");
+            }
+            println!("cargo:rustc-link-lib=z");
+        } else if cfg!(target_os = "macos") && target_os == "macos" {
+            println!("cargo:rustc-link-lib=z");
+            //TODO: remove this dependicies: CoreFoundation + Foundation
+            println!("cargo:rustc-link-lib=framework=CoreFoundation");
+            println!("cargo:rustc-link-lib=framework=Foundation");
+            println!("cargo:rustc-link-lib=framework=SystemConfiguration");
+            println!("cargo:rustc-link-lib=framework=Security");
+        }
+    }
+
+    let mut includes = vec![
+        Path::new("couchbase-lite-core").join("C").join("include"),
+        Path::new("couchbase-lite-core")
+            .join("vendor")
+            .join("fleece")
+            .join("API"),
+        Path::new("couchbase-lite-core").into(),
+        Path::new(".").into(),
+    ];
+    let target = getenv_unwrap("TARGET");
+    let mut framework_dirs = vec![];
+
+    let (mut addon_include_dirs, mut addon_framework_dirs) =
+        cc_system_include_dirs().expect("get system include directories from cc failed");
+    includes.append(&mut addon_include_dirs);
+    framework_dirs.append(&mut addon_framework_dirs);
+
+    let out_dir = getenv_unwrap("OUT_DIR");
+    let out_dir = Path::new(&out_dir);
+
+    run_bindgen_for_c_headers(
+        &target,
+        &includes,
+        &framework_dirs,
+        &[
+            "c4.h",
+            "fleece/FLSlice.h",
+            "c4Document+Fleece.h",
+            "fleece/Fleece.h",
+            "couch_lite_log_retrans.hpp",
+        ],
+        &out_dir.join("c4_header.rs"),
+    )
+    .expect("bindgen failed");
+
+    let mut cc_builder = cc::Build::new();
+
+    for inc in &includes {
+        cc_builder.include(inc);
+    }
+
+    cc_builder
+        .cpp(true)
+        .flag("-std=c++11")
+        .file("couch_lite_log_retrans.cpp")
+        .compile("couch_lite_log_retrans");
+}
 
 fn run_bindgen_for_c_headers<P: AsRef<Path>>(
     target: &str,

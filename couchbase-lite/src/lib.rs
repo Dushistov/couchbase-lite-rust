58cc00280828a7024ba0bbc481052778054e4f36
Evgeniy A. Dushistov <dushistov@mail.ru>
2022-02-04 16:44:08 +0300

    restore crate level doc comment

--- couchbase-lite/src/lib.rs
+++ couchbase-lite/src/lib.rs
@@ -1,3 +1,49 @@
+//! couchbase-lite is an ergonomic wrapper for using couchbase-lite-core from Rust.
+//! ```rust
+//! # #[macro_use]
+//! # extern crate serde;
+//! # use serde::{Serialize, Deserialize};
+//! use couchbase_lite::{
+//!     Database, DatabaseConfig, Document, kC4DB_Create,
+//!     fallible_streaming_iterator::FallibleStreamingIterator
+//! };
+//! use std::path::Path;
+//!
+//! #[derive(Serialize, Deserialize, Debug)]
+//! #[serde(tag = "type")]
+//! struct Message {
+//!     msg: String,
+//! }
+//!
+//! fn main() -> Result<(), couchbase_lite::Error> {
+//!     let mut db = Database::open_with_flags(
+//!         &std::env::temp_dir().join("a.cblite2"),
+//!         kC4DB_Create,
+//!     )?;
+//!     {
+//!         let msg = Message { msg: "Test message".into() };
+//!         let mut trans = db.transaction()?;
+//!         let enc = trans.shared_encoder_session()?;
+//!         let mut doc = Document::new(&msg, enc)?;
+//!         trans.save(&mut doc)?;
+//!         trans.commit()?;
+//!     }
+//!     println!("we have {} documents in db", db.document_count());
+//!     let query = db.query(r#"{"WHAT": ["._id"], "WHERE": ["=", [".type"], "Message"]}"#)?;
+//!     let mut iter = query.run()?;
+//!     while let Some(item) = iter.next()? {
+//!         let id = item.get_raw_checked(0)?;
+//!         let id = id.as_str()?;
+//!         let doc = db.get_existing(id)?;
+//!         println!("doc id {}", doc.id());
+//!         let db_msg: Message = doc.decode_body()?;
+//!         println!("db_msg: {:?}", db_msg);
+//!         assert_eq!("Test message", db_msg.msg);
+//!     }
+//!     Ok(())
+//! }
+//! ```
+
 mod database;
 mod doc_enumerator;
 mod document;
@@ -13,9 +59,11 @@ pub use crate::{
     database::{Database, DatabaseConfig},
     doc_enumerator::DocEnumeratorFlags,
     document::Document,
+    error::Error,
     index::IndexType,
 };
 pub use couchbase_lite_core_sys as ffi;
+pub use fallible_streaming_iterator;
 pub use ffi::{
     kC4DB_Create, kC4DB_NoUpgrade, kC4DB_NonObservable, kC4DB_ReadOnly,
     C4QueryLanguage as QueryLanguage,
